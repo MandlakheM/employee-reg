125e121a78d39e3d6954bdfe9e77d169029693cc
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-10-09 14:37:39 +0200

    added update modal for updating the admin info

--- frontend/src/components/AddAdmin.jsx
+++ frontend/src/components/AddAdmin.jsx
@@ -1,24 +1,49 @@
 import React from "react";
 import "./AdminDetails.css";
 
-const AddAdmin = ({ handleAddAdmin }) => {
+const AddAdmin = ({ handleAddAdmin, userInfo, updating }) => {
   return (
     <div className="modal">
       <div className="overlay" onClick={handleAddAdmin}></div>
       <div className="modalContent">
-        <h2>add new admin</h2>
+        <h2>{updating ? "update your profile" : "add new admin"}</h2>
         <label htmlFor="firstName">First name</label>
-        <input id="firstName" name="firstName" type="text" />
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          value={updating ? userInfo.firstName : ""}
+        />
         <label htmlFor="lastName">Last name</label>
-        <input id="lastName" name="lastName" type="text" />
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          value={updating ? userInfo.lastName : ""}
+        />
         <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={updating ? userInfo.email : ""}
+        />
         <label htmlFor="id">ID</label>
-        <input id="id" name="id" type="text" />
+        <input
+          id="id"
+          name="id"
+          type="text"
+          value={updating ? userInfo.adminID : ""}
+        />
         <label htmlFor="dob">Date of birth</label>
-        <input type="date" name="dob" id="dob" />
+        <input
+          type="date"
+          name="dob"
+          id="dob"
+          value={updating ? userInfo.dob : ""}
+        />
         <input type="file" name="" id="" />
-        <button>create admin</button>
+        <button>{updating ? "update details" : "create new admin"}</button>
         <div className="closeModal" onClick={handleAddAdmin}>
           close
         </div>
--- frontend/src/components/AdminDetails.jsx
+++ frontend/src/components/AdminDetails.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import "./AdminDetails.css";
-// import { doc, getDoc } from "firebase/firestore";
 import axios from "axios";
+import AddAdmin from "./AddAdmin";
 
 const AdminDetails = ({ handleActivate, isLoggedIn, uid }) => {
   const [userInfo, setUserInfo] = useState({});
+  const [addAdmin, setAddAdmin] = useState(false);
+  const [updating, setUpdating] = useState(false);
 
   //   useEffect(() => {
   //     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -31,50 +33,62 @@ const AdminDetails = ({ handleActivate, isLoggedIn, uid }) => {
     console.log(uid);
   }, []);
 
-  return (
-    <div className="modal">
-      <div className="overlay" onClick={handleActivate}></div>
-      <div className="modalContent">
-        <div className="picContainer">
-          <img src={userInfo.image || "https://via.placeholder.com/"} alt="" />
-        </div>
-        <p>Logged in as: {userInfo.email}</p>
-        <div>
-          <label htmlFor="firstName">First name</label>
-          <input
-            id="firstName"
-            name="firstName"
-            type="text"
-            value={userInfo.firstName}
-            readOnly
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last name</label>
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            value={userInfo.lastName}
-            readOnly
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={userInfo.email}
-            readOnly
-          />
-        </div>
+  const handleAddAdmin = () => {
+    setAddAdmin(!addAdmin);
+    setUpdating(!updating);
+  };
 
-        <div className="closeModal" onClick={handleActivate}>
-          close
+  return (
+    <>
+      <div className="modal">
+        <div className="overlay" onClick={handleActivate}></div>
+        <div className="modalContent">
+          <div className="picContainer">
+            <img
+              src={userInfo.image || "https://via.placeholder.com/"}
+              alt=""
+            />
+          </div>
+          <p>Logged in as: {userInfo.email}</p>
+          <div>
+            <label htmlFor="firstName">First name</label>
+            <input
+              id="firstName"
+              name="firstName"
+              type="text"
+              value={userInfo.firstName}
+              readOnly
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              id="lastName"
+              name="lastName"
+              type="text"
+              value={userInfo.lastName}
+              readOnly
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={userInfo.email}
+              readOnly
+            />
+          </div>
+
+          <div className="closeModal" onClick={handleActivate}>
+            close
+          </div>
+          <button onClick={handleAddAdmin}>update</button>
         </div>
       </div>
-    </div>
+      {addAdmin && <AddAdmin userInfo={userInfo} updating={updating}/>}
+    </>
   );
 };
 
